6a6b7cdddb7e08d37aa1f59f46a54e81f548381e
Yulya-Yu <your-email-address>
2018-07-05 22:57:58 +0700

    Rss Feeds suite is completed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,28 +26,40 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-
+        it('URLs not empty and defined', function() {
+            allFeeds.forEach(function(feed) {
+            expect(feed.url).toBeDefined();
+            expect(feed.url.length).not.toBe(0);
+            });
+        });
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
 
-
+        it('names not empty and defined', function() {
+            allFeeds.forEach(function(feed){
+            expect(feed.name).toBeDefined();
+            expect(feed.name.length).not.toBe(0);
+            });
+        });
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
     });
 
-
+    describe('The Menu', function() {
     /* TODO: Write a new test suite named "The menu" */
-
+        it('menu hidden by default', function() {
+            expect(body.hasClass('menu-hidden')).toBe(true);
+         }); 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-
+        it('menu visible on click', function() {
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
