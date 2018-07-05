df765197c3ea76ef3d2c77dd27c21c8f53543b77
Yulya-Yu <your-email-address>
2018-07-05 23:19:08 +0700

    New Feed Selection suite is added

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -73,19 +73,42 @@ $(function() {
            icon.click();
             expect(body.hasClass('menu-hidden')).toBe(true);
         });
-    /* TODO: Write a new test suite named "Initial Entries" */
 
+    });
+    /* TODO: Write a new test suite named "Initial Entries" */
+ describe('Initial Entries', function() {
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-
+    });
     /* TODO: Write a new test suite named "New Feed Selection" */
+ describe('New Feed Selection', function() {
+    let contentBefore;
+    let contentAfter;
 
+        beforeEach(function( done ) {
+            loadFeed(1, function() {
+            contentBefore = ('.feed').html();
+            done();
+            });
+        });
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        it('loaded new feed', function(done){
+        loadFeed(2, function() {
+        contentAfter = ('.feed').html();
+        expect('contentAfter').not.toBe(contentBefore);
+        done();
+        });
+      });
+    });
+
 }());
